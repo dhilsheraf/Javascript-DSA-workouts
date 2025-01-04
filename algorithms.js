d0d3e494757b68953d0935220729758408f50086
dhilsheraf <dhilsherafummer@gmail.com>
2025-01-04 15:37:37 +0530

    first commit

---
+++ algorithms.js
@@ -0,0 +1,37 @@
+//mathematical algorithms for learn basic
+function fibonacci(n){
+    let arr = [0,1]
+    for(let i = 2 ; i < n ; i++){
+         arr[i] = arr[i-1] + arr[i-2]
+    }
+    return arr
+}
+
+const val = fibonacci(1);
+// console.log(val)
+
+// Big-O = o(n)
+
+//factorila of a number 
+
+function factorial(n){
+    let result = 1
+    for(let i = 1 ; i <= n ; i++){
+        result = result*i
+    }
+    return result
+}
+// console.log(factorial(5))
+
+// prime number 
+
+function isPrime(n){
+    if(n<2) return false
+    for(let i = 2 ; i <= Math.sqrt(n) ; i++ ){
+        if(n % i == 0) return false
+    }
+    return true
+}
+console.log(isPrime(1));
+console.log(isPrime(10));
+console.log(isPrime(5));
---
+++ memoryAllocationLeak.js
@@ -0,0 +1,18 @@
+//this is example and workouts of memory allocation and memory leak with example
+
+
+//Variables declared without let, const, or var are attached to the global object (window in browsers).
+
+function createLeak() {
+    leakedVar = "I'm a leak!"; // Implicit global variable
+}
+createLeak();
+
+
+//Forgotten Timers or Callbacks
+//setInterval or setTimeout keeps a reference to its callback, which can prevent associated objects from being garbage collected.
+
+function startTimer() {
+    setInterval(() => console.log("Still running"), 1000);
+}
+startTimer(); // Timer runs forever unless explicitly cleared
